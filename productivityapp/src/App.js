45ce0af55b3e8487784bb47965dfbaa38b8f3b28
Jonathan <jawnscott123@gmail.com>
2021-02-13 15:50:54 -0700

    Added authentication

--- productivityapp/src/App.js
+++ productivityapp/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import Login from './components/session/Login';
+import fire from './fire.js';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+    fire.auth().onAuthStateChanged((user) => {
+      return user ? setIsLoggedIn(true) : setIsLoggedIn(false);
+  });
+  
+  console.log('logged in?', isLoggedIn);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        
+        {!isLoggedIn
+          ? (
+            <>
+              <Switch>
+                <Route path="/">
+                  <Login />
+                </Route>
+              </Switch>
+            </>
+          ) 
+          : (
+          <span onClick={signOut}>
+            <a href="#">Sign out</a>
+          </span>
+          )}
+      </Router>
     </div>
   );
 }
+const signOut = () => {
+  fire.auth().signOut()
+};
 
 export default App;
