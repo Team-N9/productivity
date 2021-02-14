27b884acd9434e4d5a753152bd67e068aca67d61
Sysimq <52726283+Sysimq@users.noreply.github.com>
2021-02-13 19:32:03 -0700

    todo_list

--- productivityapp/src/App.js
+++ productivityapp/src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import Typography from "@material-ui/core/Typography";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+
+const LOCAL_STORAGE_KEY = "react-todo-list-todos";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    // fires when app component mounts to the DOM
+    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storageTodos) {
+      setTodos(storageTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    // fires when todos array gets updated
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  function addTodo(todo) {
+    // adds new todo to beginning of todos array
+    setTodos([todo, ...todos]);
+  }
+
+  function toggleComplete(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+        return todo;
+      })
+    );
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography style={{ padding: 16 }} variant="h1">
+        Todo List
+      </Typography>
+      <TodoForm addTodo={addTodo} />
+      <TodoList
+        todos={todos}
+        removeTodo={removeTodo}
+        toggleComplete={toggleComplete}
+      />
     </div>
   );
 }
---
+++ productivityapp/src/components/TodoForm.js
@@ -0,0 +1,41 @@
+import { Button, TextField } from "@material-ui/core";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+function TodoForm({ addTodo }) {
+  const [todo, setTodo] = useState({
+    id: "",
+    task: "",
+    completed: false
+  });
+
+  function handleTaskInputChange(e) {
+    // e.target.value contains new input from onChange
+    // event for input elements
+    setTodo({ ...todo, task: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault(); // prevents browser refresh
+    // trim() gets rid of string whitespace
+    if (todo.task.trim()) {
+      addTodo({ ...todo, id: uuidv4() });
+      setTodo({ ...todo, task: "" });
+    }
+  }
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <TextField
+        label="Task"
+        type="text"
+        name="task"
+        value={todo.task}
+        onChange={handleTaskInputChange}
+      />
+      <Button type="submit">Submit</Button>
+    </form>
+  );
+}
+
+export default TodoForm;
