1787e4b5f9f2cb4395410fd689c76d16bf1e2399
plklinh <plekhanhlinh12345@gmail.com>
2021-02-13 22:14:09 -0700

    updated timer logic

--- productivityapp/src/components/timer_component.js
+++ productivityapp/src/components/timer_component.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useState, useEffect, useRef } from 'react';
+import { useState,  useRef } from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
@@ -11,10 +12,40 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import { Container } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    timer_container: {
+        backgroundColor: '#EEEEFF',
+        margin: '0 auto',
+        width: '500px',
+        height: '500px',
+        position: 'relative',
+        borderRadius: '50%',
+      },
+    h3: {
+        textAlign: 'center',
+        paddingTop: '8px',
+        letterSpacing: '1.2px',
+        fontWeight: 500,
+      },
+    timer_card: {
+        position: 'absolute',
+        textAlign: 'center',
+        backgroundColor: 'white',
+        width: '325px',
+        height: '130px',
+        top: '110px',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+      },
+  }));
+  
 
 const Timer = () => {
-    const [time, setTimer] = useState(0)
-    const [activity, setActivity] = React.useState('')
+    const classes = useStyles();
+    const [timer, setTimer] = useState(0)
+    const [activity, setActivity] = React.useState('Study')
     const [isActive, setIsActive] = useState(false)
     const [canPause, setCanPause] = useState(false)
     const [isComplete, setIsComplete] = useState(false)
@@ -45,39 +76,38 @@ const Timer = () => {
     }
 
     const handleComplete = () => {
-        if (activity.length == 0){
-            
-        } else {
-            
-        }
+        handlePause()
+        setIsComplete(true)
     }
 
     const handleYes = () => {
+        setIsComplete(false)
+        
         clearInterval(countRef.current)
-        setActivity('')
+        setActivity('Study')
         setIsActive(false)
         setCanPause(false)
         setTimer(0)
     }
 
     const handleNo = ( ) => {
-
+        setIsComplete(false)
     }
 
     const formatTime = () => {
-        const getSeconds = `0${(time % 60)}`.slice(-2)
+        const getSeconds = `0${(timer % 60)}`.slice(-2)
 
-        const minutes = `${Math.floor(time / 60)}`
+        const minutes = `${Math.floor(timer / 60)}`
         const getMinutes = `0${minutes % 60}`.slice(-2)
 
-        const getHours = `0${Math.floor(time / 3600)}`.slice(-2)
+        const getHours = `0${Math.floor(timer / 3600)}`.slice(-2)
     
         return `${getHours} : ${getMinutes} : ${getSeconds}`
     }
 
     return (
-        <div className="Actitivity Timer">
-            <h3>Timer</h3>
+        <div className= {classes.timer_container}>
+            <h3 className={classes.h3}>Timer</h3>
             <InputLabel id="activityName">Name</InputLabel>
             <Select labelId="activityName" 
                 id="select" 
@@ -85,9 +115,10 @@ const Timer = () => {
                 onChange= {handleChange} >
                 <MenuItem value="Study">Study</MenuItem>
                 <MenuItem value="Personal Project">Personal Project</MenuItem>
+                <MenuItem value="Misc">Misc</MenuItem>
             </Select>
-            <div className='stopwatch-card'>
-                <p>{formatTime(time)}</p> 
+            <div className={classes.timer_card}>
+                <p>{formatTime(timer)}</p> 
                 <div className='Buttons'>
                     {!isActive && !canPause ?
                         <Button onClick={handleStart}>Start</Button>
@@ -104,9 +135,10 @@ const Timer = () => {
                 aria-labelledby="confirm-dialog-title"
                 aria-describedby="confirm-dialog-description"
             >
-                <DialogTitle id="confirm-dialog-title">{"Save Activity"}</DialogTitle>
+                <DialogTitle id="confirm-dialog-title"> Save Activity </DialogTitle>
                 <DialogContent>
                 <DialogContentText id="confirm-dialog-description">
+                    Activity: {activity}, Duration: {formatTime(timer)} {"\n"}
                    Do you want to save this activity? 
                 </DialogContentText>
                 </DialogContent>
