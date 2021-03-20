630adaf075bbb0403ed41195c682cf3d63ad153f
The-Real-Guy <aasdimaranan@gmail.com>
2021-03-19 21:40:11 -0600

    Separated frontend from backend

--- backend/routes/timers.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getTimers, addTimer } = require('../controllers/timerController');
-
-router.route('/').get(getTimers).post(addTimer);
-
-module.exports = router;
